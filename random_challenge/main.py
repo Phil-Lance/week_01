da8668479aab9f0d348323f7cc212ac63bf6481a
Phil-Lance <Philippelalancette@gmail.com>
2025-07-23 22:22:44 -0400

    revision 1.0.1

--- random_challenge/main.py
+++ random_challenge/main.py
@@ -3,8 +3,8 @@ import challenge_dict
 
 
 
-def get_random_challenge():
-    difficulty = input("Enter difficulty level (beginner (B), intermediate (I), advanced (A)): ").upper()
+def get_random_challenge() -> None:
+    difficulty: str = input("Enter difficulty level (beginner (B), intermediate (I), advanced (A)): ").upper()
     if difficulty not in ['B', 'I', 'A']:
         print("Invalid difficulty level. Please enter B, I, or A.")
         difficulty = input("Enter difficulty level (beginner (B), intermediate (I), advanced (A)): ").upper()
@@ -12,10 +12,10 @@ def get_random_challenge():
     elif difficulty == 'B':
         random_number = random.randint(1, 15)
     else:
-        random_number = random.randint(1, 20)
+        random_number: int = random.randint(1, 20)
     weekly_challenge:str = difficulty + str(random_number)
     return print(f"Your weekly challenge is: {challenge_dict.challenge_dict.get(weekly_challenge)}  ({weekly_challenge})")
 
-week_challenge = get_random_challenge()
+week_challenge: None = get_random_challenge()
 if __name__ == "__main__":
     print(week_challenge)
